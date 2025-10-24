71cfb3af847d1fd709c0578eedb808d6a93dc630
laurabas05 <laaurabas@gmail.com>
2025-10-24 19:56:55 +0200

    Primer commit

---
+++ posts/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+from .models import Post
+
+def post_list(request):
+    posts = Post.objects.all()
+    return render(request, 'posts/posts.html', {'posts': posts})
+
+def post_detail(request, slug):
+    post = Post.objects.get(slug=slug)
+    return render(request, 'posts/post_detail.html', {'post': post})
