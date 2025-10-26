4bb1b5b84884356b88fc6ac14028743b093c1233
laurabas05 <laaurabas@gmail.com>
2025-10-26 15:29:31 +0100

    Resuelta Task 8 añadiendo otras vistas y mejorando el diseño

--- users/urls.py
+++ users/urls.py
@@ -3,5 +3,8 @@ from . import views
 
 app_name = 'users'
 urlpatterns = [
-    path('', views.register_view, name='register')
+    path('profile/', views.profile, name='profile'),
+    path('register/', views.register_view, name='register'),
+    path('login/', views.login_view, name='login'),
+    path('logout/', views.logout_user, name='logout')
 ]
